992d45d8c180f5053f7c3d4f1a7f5e6739359426
dbenoot <dbenoot@penguin>
2020-01-16 09:37:22 +0100

    search, unique searching + partial recognition not working

--- search.go
+++ search.go
@@ -52,14 +52,16 @@ func searchEntry(dir string, v bool, text string, tag string, ingredients string
 		// TEXT
 		// loop over result and remove unsuitable records
 
-		/* 		for r  := range result {
+		for r := range result {
 			if !strings.Contains(v.Body, text) {
 				result, _ = remove(result, r)
 			}
-		} */
+		}
 
-		fmt.Println(result)
 	}
+
+	fmt.Println(result)
+
 }
 
 func subslice(s1 []string, s2 []string) bool {
