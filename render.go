6886784eab7222173e543910ecdb64c8679731fb
Donald Benoot <dbenoot@gmail.com>
2020-02-05 16:23:32 +0100

    basic rendering works

--- render.go
+++ render.go
@@ -5,10 +5,12 @@ import (
 	"fmt"
 	"html"
 	"html/template"
+	"io"
 	"io/ioutil"
 	"os"
 	"path/filepath"
 
+	"gopkg.in/russross/blackfriday.v2"
 	"gopkg.in/yaml.v2"
 )
 
@@ -32,6 +34,10 @@ func render(dir string, b string, r string) {
 		fmt.Println("Book does not exist.")
 	}
 
+	if _, err := os.Stat(filepath.Join(dir, "rendered", b, "images")); os.IsNotExist(err) {
+		_ = os.MkdirAll(filepath.Join(dir, "rendered", b, "images"), 0755)
+	}
+
 	data, err := ioutil.ReadFile(file)
 	err = yaml.Unmarshal([]byte(data), &cookbook)
 	check(err)
@@ -51,9 +57,41 @@ func render(dir string, b string, r string) {
 
 	for _, v := range selectedRecipes {
 
+		// instance a bufferstring
+
 		u := bytes.NewBufferString("")
 
-		t.Execute(u, map[string]string{"recipetitle": v.Title, "recipeingredients": string(v.Preptime), "recipebody": v.Body})
+		// preprocess the ingredientslist to a table
+
+		var ing = "<table>"
+		for k, v := range v.Ingredients {
+			ing = ing + "<tr><td>" + k + "</td><td>" + v + "</td></tr>"
+		}
+		ing = ing + "</table>"
+
+		// get image path
+
+		var img string
+		if len(v.Image) > 0 {
+			img = filepath.Join("images", v.Image)
+			img = "<img src=\"" + img + "\">"
+			_, err = copy(filepath.Join(dir, "recipes", "images", v.Image), filepath.Join(dir, "rendered", b, "images", v.Image))
+			check(err)
+		}
+		// create the page
+
+		t.Execute(u, map[string]interface{}{
+			"recipetitle":       v.Title,
+			"recipeingredients": ing,
+			"recipeimage":       img,
+			"recipebody":        string(blackfriday.Run([]byte(v.Body))),
+			"preptime":          v.Preptime,
+			"cooktime":          v.Cooktime,
+			"origin":            v.Origin,
+			"tags":              v.Tags,
+		})
+
+		// add the rendered page to the HTML body of the book
 
 		body = body + u.String()
 	}
@@ -69,11 +107,48 @@ func render(dir string, b string, r string) {
 
 	// write u to file
 
-	f, err := os.Create(filepath.Join(dir, "rendered", b+".html"))
+	bookdir := filepath.Join(dir, "rendered", b)
+
+	if _, err := os.Stat(bookdir); os.IsNotExist(err) {
+		err = os.MkdirAll(bookdir, 0755)
+		check(err)
+	}
+
+	f, err := os.Create(filepath.Join(bookdir, b+".html"))
 	check(err)
 	defer f.Close()
 
 	_, err = f.Write(u.Bytes())
 	check(err)
 
+	if _, err := os.Stat(filepath.Join(bookdir, "book.css")); os.IsNotExist(err) {
+		_, err = copy(filepath.Join(dir, "templates", "book.css"), filepath.Join(bookdir, "book.css"))
+		check(err)
+	}
+
+}
+
+func copy(src, dst string) (int64, error) {
+	sourceFileStat, err := os.Stat(src)
+	if err != nil {
+		return 0, err
+	}
+
+	if !sourceFileStat.Mode().IsRegular() {
+		return 0, fmt.Errorf("%s is not a regular file", src)
+	}
+
+	source, err := os.Open(src)
+	if err != nil {
+		return 0, err
+	}
+	defer source.Close()
+
+	destination, err := os.Create(dst)
+	if err != nil {
+		return 0, err
+	}
+	defer destination.Close()
+	nBytes, err := io.Copy(destination, source)
+	return nBytes, err
 }
